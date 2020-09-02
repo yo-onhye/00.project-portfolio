69a93c529c4017fc0bd6e62f6ad89cb00afce1ba
yo-onhye <woony0807@gmail.com>
2020-09-02 20:00:11 +0900

    projcetheader영역 추가

--- project-portfolio/src/App.js
+++ project-portfolio/src/App.js
@@ -14,14 +14,42 @@ class App extends Component {
 		}
 	};
 
+	LogoJs = () => {
+		const elLogo = document.querySelector(".projectLogo");
+		let sLogotxt = elLogo.innerText;
+		let nRandom1 = Math.floor(Math.random() * (sLogotxt.length - 0));
+		let nRandom2 = Math.floor(Math.random() * (sLogotxt.length - 0));
+		let nRandom3 = Math.floor(Math.random() * (sLogotxt.length - 0));
+		let index = 0;
+		elLogo.innerText = "";
+
+		for (let i of sLogotxt) {
+			let newSpan = document.createElement("span");
+			newSpan.classList.add("logo" + index);
+			newSpan.innerText = i;
+			elLogo.appendChild(newSpan);
+			if ((index === nRandom1) | (index === nRandom2) | (index === nRandom3)) {
+				document.querySelector(".logo" + index).classList.add("blink");
+			}
+			index++;
+		}
+	};
+
 	componentDidMount() {
 		this.backgroundJs();
+		this.LogoJs();
 	}
 
 	render() {
 		return (
 			<div className='projectWrap'>
-				<header className='projcetheader'></header>
+				<header className='projcetheader'>
+					<h1 className='projectLogo'>
+						<span className='words'>FRONT-END</span>
+						<span className='words'>DEVELOPER</span>
+					</h1>
+					<p className="projectTitle">publisher & front-end developer</p>
+				</header>
 				<section className='projectSection'></section>
 			</div>
 		);
